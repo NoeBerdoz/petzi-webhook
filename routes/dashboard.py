933bbaf246273de2f541fd1d0d630f3d8ba2dbe4
NoeBerdoz <berdoznoe@gmail.com>
2025-02-10 16:32:45 +0100

    Add pagination and events page

--- routes/dashboard.py
+++ routes/dashboard.py
@@ -1,6 +1,8 @@
+import math
+
 import psycopg2
 import psycopg2.extras
-from flask import render_template, Blueprint
+from flask import render_template, Blueprint, request
 
 from persistence.database import Database
 from service.tickets import load_chart_data
@@ -48,12 +50,63 @@ def get_tickets():
             )
             tickets = [dict(row) for row in cur.fetchall()]
 
+            page = request.args.get('page', 1, type=int)  # Get current page from query params
+            per_page = 30
+            total_pages = math.ceil(len(tickets) / per_page)
+
+            start = (page - 1) * per_page
+            end = start + per_page
+
+            paginated_data = tickets[start:end]
+
             chart_data = load_chart_data()
 
             return render_template(
                 'tickets.html',
                 title="Tickets",
-                data=tickets,
+                data=paginated_data,
+                page=page,
+                total_pages=total_pages,
+                chart_categories=chart_data["categories"],
+                chart_sales=chart_data["sales"],
+            )
+
+@dashboard_blueprint.route('/events')
+def get_events():
+    with Database.get_db_connection() as conn:
+        with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as cur:
+            cur.execute(
+                """
+                    SELECT 
+                        e.id,
+                        e.event_id,
+                        e.name,
+                        COUNT(t.id) AS ticket_count
+                    FROM events e
+                    LEFT JOIN tickets t ON e.event_id = t.event_id
+                    GROUP BY e.id, e.event_id, e.name
+                    order by e.id;
+                """
+            )
+            events = [dict(row) for row in cur.fetchall()]
+
+            page = request.args.get('page', 1, type=int)  # Get current page from query params
+            per_page = 30
+            total_pages = math.ceil(len(events) / per_page)
+
+            start = (page - 1) * per_page
+            end = start + per_page
+
+            paginated_data = events[start:end]
+
+            chart_data = load_chart_data()
+
+            return render_template(
+                'events.html',
+                title="Events",
+                data=paginated_data,
+                page=page,
+                total_pages=total_pages,
                 chart_categories=chart_data["categories"],
                 chart_sales=chart_data["sales"],
             )
