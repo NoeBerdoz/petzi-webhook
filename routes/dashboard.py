865eb9bbd23a076a9285b4c3429a46b353503876
NoeBerdoz <berdoznoe@gmail.com>
2025-02-11 20:45:07 +0100

    Add select event to export functionality

--- routes/dashboard.py
+++ routes/dashboard.py
@@ -14,27 +14,10 @@ dashboard_blueprint = Blueprint('dashboard', __name__, url_prefix='/dashboard')
 def get_home():
     with Database.get_db_connection() as conn:
         with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as cur:
-            # Fetch buyers
-            cur.execute(
-                """
-                   SELECT id, role, first_name, last_name, postcode FROM buyers;
-               """)
-            buyers = [dict(row) for row in cur.fetchall()]
+            cur.execute("SELECT event_id, name FROM events;")
+            events = cur.fetchall()
 
-            # Fetch tickets
-            cur.execute(
-                """
-                   SELECT id, number, type, title, category, event_id, cancellation_reason, generated_at, promoter, price_amount, price_currency, buyer_id FROM tickets;
-               """)
-            tickets = [dict(row) for row in cur.fetchall()]
-
-            # Merge buyers and tickets
-            data = []
-            for buyer in buyers:
-                buyer_tickets = [t for t in tickets if t["buyer_id"] == buyer["id"]]
-                data.append({"buyer": buyer, "tickets": buyer_tickets})
-
-    return render_template('home.html', data=data, title="My Dashboard")
+    return render_template('home.html', events=events, title="Accueil")
 
 
 @dashboard_blueprint.route('/tickets')
