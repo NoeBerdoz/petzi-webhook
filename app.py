c9965b2e3c1e1a590592111527e24988602711ac
NoeBerdoz <berdoznoe@gmail.com>
2025-02-12 23:20:34 +0100

    Explain werkzeug secret key

--- app.py
+++ app.py
@@ -11,10 +11,9 @@ database = Database()
 app = Flask(__name__)
 app.register_blueprint(dashboard_blueprint)
 app.register_blueprint(api_blueprint)
-
 app.register_blueprint(auth_blueprint)
 
 if __name__ == "__main__":
     database.create_tables()
-    app.secret_key = 'I want to play guitar'
+    app.secret_key = 'I want to play guitar' # Used by werkzeug to manage authentication
     app.run(host="0.0.0.0", port=5000, debug=True)
